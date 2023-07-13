aef7326d0492204de7787b4a5fe4920339dee7f8
Eslam Sabry <eslam2001sabry@gmail.com>
2023-07-14 01:48:24 +0300

    finish email form

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {BehaviorSubject, Observable} from "rxjs";
 
@@ -6,5 +7,8 @@ import {BehaviorSubject, Observable} from "rxjs";
 })
 export class DataService {
   rtlDir = new BehaviorSubject(false);
-  constructor() { }
+  constructor(private _HttpClient: HttpClient) { }
+  sendEmail(model:any):Observable<any> {
+    return this._HttpClient.post('https://api.emailjs.com/api/v1.0/email/send', model);
+  }
 }
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -36,5 +36,30 @@ export class ContactComponent implements OnInit{
   }
 
   sendEmail() {
+    if(this.emailForm.valid){
+      const data = {
+        service_id: 'service_mawlpev',
+        template_id: 'template_ljw4099',
+        user_id: '3tPTvM0H5SQeHRa_P',
+        template_params: {
+          from_name: this.emailForm.value.name,
+          from_subject: this.emailForm.value.subject,
+          from_message: this.emailForm.value.message,
+          from_email: this.emailForm.value.email,
+        }
+      };
+      this._dataService.sendEmail(data).subscribe({
+        next: (response) => {
+          console.log(response);
+          // this.toastr.success(!this.rtlDir?`Password changed successfully!`:`تم تغيير كلمة المرور بنجاح`);
+          // this.router.navigate(['/home']);
+        },
+        error : (error)=> {
+          console.log(error);
+          // if(error.error.data == 'wrong old password'){
+          //   this.toastr.error(!this.rtlDir?`Wrong old password`:`كلمة المرور الحالية خاطئة` )
+          }
+      })
+    }
   }
 }
